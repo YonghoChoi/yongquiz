90502fc5402002890b9e656382e950cab638f66a
YonghoChoi <yongho1037@gmail.com>
2021-01-23 01:30:10 +0900

    Quiz 메인 페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import MainPage from "./components/MainPage.vue";
+import MainPage from "./views/MainPage.vue";
 
 export default {
   name: "App",
@@ -22,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="hzt-center title service-name-text">Qube</div>
+      <div class="hzt-center subtitle game-mode-text">Single Player game</div>
+      <div class="hzt-center quiz-subject">
+        <span class="center quiz-text">Qube 만들기</span>
+      </div>
+    </div>
+
+    <div class="content">
+      <div class="hzt-center join">
+        <span class="hzt-center subtitle join-game-text">Join the game</span>
+        <div class="hzt-center nickname">
+          <input type="text" id="nickname" />
+          <button id="btn-join">join</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 400px 400px;
+}
+
+.header {
+  position: relative;
+  background-color: #47178f;
+  width: 100%;
+  height: 350px;
+}
+
+.content {
+  position: relative;
+  background-color: #25076b;
+  width: 100%;
+  height: 400px;
+}
+
+.center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.vtc-center {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.hzt-center {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.join {
+  top: 150px;
+  background-color: #864bbf;
+  width: 400px;
+  height: 150px;
+}
+
+.title {
+  color: rgb(255, 255, 255);
+  font-size: 3rem;
+  font-weight: 600;
+}
+
+.subtitle {
+  color: rgb(255, 255, 255);
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+
+.service-name-text {
+  top: 50px;
+}
+
+.game-mode-text {
+  top: 120px;
+}
+
+.join-game-text {
+  top: 30px;
+}
+
+.nickname {
+  position: absolute;
+  display: flex;
+  top: 80px;
+}
+
+.quiz-subject {
+  top: 230px;
+  background-color: rgb(255, 255, 255);
+  width: 600px;
+  height: 150px;
+  z-index: 1;
+}
+
+.quiz-text {
+  color: black;
+  font-size: 3rem;
+  font-weight: 600;
+}
+</style>
